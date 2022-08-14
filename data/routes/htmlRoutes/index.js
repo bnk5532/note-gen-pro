5a9adbce445544f7aafd4bff8429ea82148ab69e
Brian King <bnk5532@gmail.com>
2022-08-13 22:05:53 -0400

    delete route added

--- data/routes/htmlRoutes/index.js
+++ data/routes/htmlRoutes/index.js
@@ -6,8 +6,24 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// router.delete(/api.notes:id), (req, res) => {
+//   const result = findById(req.params.id, notes);
+//   if (result) {
+//     res.json
+//   }
+// }
+
+router.delete("/notes/:id", (req, res) => {
+  var deleteId = request.params.deleteId;
+ 
+  request.db.get('notes').remove({'id': deleteId}, function(error, document) {
+   if (error) response.send(error);
+   return response.send("deleted");
+  });
+ });
+
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
-module.exports = router; 
+module.exports = router; /Users/BNKing/Documents/Projects/note-gen-pro/data/public/index.html
